8a2449b08e7ebf03cf134609a061623c027e254d
matt venn <matt@mattvenn.net>
2024-08-16 22:20:49 +0200

    remove downloaded csv

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cp ~/Downloads/supabase_aouvjyxihpudhbmbuepk_shuttle\ progress\ graph\ data.csv data.csv
+csv_file=~/Downloads/supabase_aouvjyxihpudhbmbuepk_shuttle\ progress\ graph\ data.csv 
+cp $csv_file data.csv
+rm $csv_file
 . venv/bin/activate
 python3 parse.py
