44b45965325166305c914e0904cd0bf0471dc0f0
matt venn <matt@mattvenn.net>
2024-08-16 22:26:27 +0200

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-csv_file=~/Downloads/supabase_aouvjyxihpudhbmbuepk_shuttle\ progress\ graph\ data.csv 
-cp $csv_file data.csv
-rm $csv_file
+csv_file=~/Downloads/supabase_aouvjyxihpudhbmbuepk_shuttle\ progress\ graph\ data.csv
+cp "$csv_file" data.csv
+rm "$csv_file"
 . venv/bin/activate
 python3 parse.py
